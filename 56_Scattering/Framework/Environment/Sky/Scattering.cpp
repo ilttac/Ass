5b741a74161c200b1390ead24c0a8dba896279a8
ilttac <whdals5607@naver.com>
2020-05-27 19:48:45 +0900

    56_02

--- 56_Scattering/Framework/Environment/Sky/Scattering.cpp
+++ 56_Scattering/Framework/Environment/Sky/Scattering.cpp
@@ -1,2 +1,59 @@
 #include "Framework.h"
 #include "Scattering.h"
+
+Scattering::Scattering(Shader* shader)
+	: shader(shader),width(128),height(64)
+{
+	mieTarget = new RenderTarget(width, height, DXGI_FORMAT_R32G32B32A32_FLOAT);
+	rayleighTarget = new RenderTarget(width, height, DXGI_FORMAT_R32G32B32A32_FLOAT);
+	depthStencil = new DepthStencil(width, height);
+	viewport = new Viewport((float)width, (float)height);
+
+	render2D = new Render2D();
+	render2D->GetTransform()->Scale(200, 200, 1);
+	render2D->GetTransform()->RotationDegree(0, 0, -90);
+
+	CreateQuad();
+}
+
+Scattering::~Scattering()
+{
+	SafeDelete(render2D);
+	SafeDelete(mieTarget);
+	SafeDelete(rayleighTarget);
+	SafeDelete(depthStencil);
+	SafeDelete(viewport);
+
+	SafeDelete(vertexBuffer);
+}
+
+void Scattering::Update()
+{
+	if(bDebug == true)
+		render2D->Update();
+}
+
+void Scattering::Render()
+{
+}
+
+void Scattering::PreRender()
+{
+	RenderTarget* rtvs[2];
+	rtvs[0] = rayleighTarget;
+	rtvs[1] = mieTarget;
+	RenderTarget::Sets(rtvs, 2, depthStencil);
+
+	viewport->RSSetViewport();
+	D3D::GetDC()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	shader->Draw(0, pass, 6);
+}
+
+void Scattering::PostRender()
+{
+}
+
+void Scattering::CreateQuad()
+{
+}
--- 56_Scattering/Framework/Environment/Sky/Scattering.h
+++ 56_Scattering/Framework/Environment/Sky/Scattering.h
@@ -3,7 +3,16 @@
 class Scattering
 {
 public:
+	Scattering(Shader* shader);
+	~Scattering();
 
+	void Update();
+	void Render();
+	void PreRender();
+	void PostRender();
+	
+	void Pass(UINT val) { pass = val; }
+	void SetDebug(bool val) { bDebug = val; }
 private:
 	void CreateQuad(); //ScreenNdc
 
@@ -12,4 +21,14 @@ private:
 	bool bDebug = false;
 	UINT pass = 0;
 
+	const UINT width, height;
+
+	Shader* shader;
+	Render2D* render2D;
+
+	RenderTarget* mieTarget, *rayleighTarget;
+	DepthStencil* depthStencil;
+	Viewport* viewport;
+
+	VertexBuffer* vertexBuffer;
 };
